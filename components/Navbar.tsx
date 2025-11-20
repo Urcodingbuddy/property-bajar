614f4e26a69b1a66ebdc862ef67b6f70bbd35d06
Urcodingbuddy <anshpethe110@gmail.com>
2025-11-20 12:47:31 +0530

    Refector : menu button and width alignment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,8 +9,8 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-linear-to-r from-[#8812FC] to-[#6D10F4] shadow-lg">
-      <div className="px-50 mx-auto">
-        <div className="flex items-center justify-between h-16">
+      <div className="max-w-340 mx-auto">
+        <div className="flex items-center justify-between h-16 px-0 md:px-8">
           <div className="w-50">
             <img src="./logo.png" alt="" />
           </div>
@@ -58,21 +58,6 @@ export default function Header() {
           </div>
 
           <div className="flex lg:hidden items-center gap-2">
-            <button className="p-2 text-white rounded-lg transition">
-              <svg
-                className="w-5 h-5 cursor-pointer"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              </svg>
-            </button>
             <button
               onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
               className="p-2 text-white hover:bg-purple-500 rounded-lg transition"
@@ -80,7 +65,19 @@ export default function Header() {
               {mobileMenuOpen ? (
                 <X className="w-6 h-6" />
               ) : (
-                <Menu className="w-6 h-6" />
+                <svg
+                  className="w-6.5 h-6.5 cursor-pointer"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
               )}
             </button>
           </div>
